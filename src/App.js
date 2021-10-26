b9f4060d5cb15b50d8a17bdc9d0a0a447edf381a
Nosov-Pavel <nopaev@gmail.com>
2021-10-25 22:02:44 -0400

    todo1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
+
+  const unitialState = [
+      {
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+      },
+      {
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+      },
+      {
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+      },
+  ]
 function App() {
+    const [todos, setTodos] = useState(unitialState)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ol>
+        {todos.map((el => <li key={el.id}>{el.title}</li>))}
+        </ol>
+
     </div>
   );
 }
