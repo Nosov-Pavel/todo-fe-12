08ddafd3c7dfd9c4af8ade69fdd85453c279a8b4
Nosov-Pavel <nopaev@gmail.com>
2021-10-26 20:58:32 -0400

    todo1+button

--- src/App.js
+++ src/App.js
@@ -2,34 +2,41 @@ import './App.css';
 import {useState} from "react";
 
 
-  const unitialState = [
-      {
-        "id": 1,
+const unitialState = [
+    {
+        "id": 16,
         "title": "delectus aut autem",
         "completed": false
-      },
-      {
-        "id": 2,
+    },
+    {
+        "id": 23,
         "title": "quis ut nam facilis et officia qui",
         "completed": false
-      },
-      {
-        "id": 3,
+    },
+    {
+        "id": 37,
         "title": "fugiat veniam minus",
         "completed": false
-      },
-  ]
+    },
+]
+
 function App() {
     const [todos, setTodos] = useState(unitialState)
 
-  return (
-    <div className="App">
-        <ol>
-        {todos.map((el => <li key={el.id}>{el.title}</li>))}
-        </ol>
+    return (
+        <div className="App">
+            <ol>
+                {todos.map(el => <li key={el.id}>
+        <span style={el.completed ? {textDecoration: "line-through"} : {}} >
+            {el.id} {el.title} {el.completed}
+            </span>
+            </li>)}
+        <button>DELETE</button>
+            <button> </button>
+                </ol>
 
-    </div>
-  );
-}
+                    </div>
+                    );
+                }
 
-export default App;
+                export default App;
